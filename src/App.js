5e0a36aeff2dde3e7bb7114820940b2147dfcda7
Sreekar Reddy Aparacherla <sreekar.reddy.aparacherla@gmail.com>
2021-02-17 23:53:07 +0800

    add editing functionality

--- src/App.js
+++ src/App.js
@@ -1,244 +1,270 @@
-import React, { Component, useState } from 'react';
+import React, {Component, useState} from 'react';
 import Device from './Component/Devices/Device/Device.component'
 import classes from './App.module.css'
 import SavedDevices from './Component/SavedDevices/SavedDevice.component'
 
-class App extends Component{
-  state = {
-    devices: [{
-    "id": 1,
-    "device": "Derpies",
-    "os": "Android 4.3",
-    "manufacturer": "Motorola",
-    "lastCheckedOutDate": "2016-02-21T09:10:00-05:00",
-    "lastCheckedOutBy": "Chris Evans",
-    "isCheckedOut": false
-    },
-    {
-      "id": 2,
-      "device": "Moto G",
-      "os": "Android 4.3",
-      "manufacturer": "Motorola",
-      "lastCheckedOutDate": "2016-02-21T09:10:00-05:00",
-      "lastCheckedOutBy": "Chris Evans",
-      "isCheckedOut": false
-      },
-    {
-      "id": 3,
-      "device": "Moto G",
-      "os": "Android 4.3",
-      "manufacturer": "Motorola",
-      "lastCheckedOutDate": "2016-02-21T09:10:00-05:00",
-      "lastCheckedOutBy": "Chris Evans",
-      "isCheckedOut": false
-    },
-    {
-      "id": 4,
-      "device": "Moto G",
-      "os": "Android 4.3",
-      "manufacturer": "Motorola",
-      "lastCheckedOutDate": "2016-02-21T09:10:00-05:00",
-      "lastCheckedOutBy": "Chris Evans",
-      "isCheckedOut": false
-    },
-    {
-      "id": 5,
-      "device": "Moto G",
-      "os": "Android 4.3",
-      "manufacturer": "Motorola",
-      "lastCheckedOutDate": "2016-02-21T09:10:00-05:00",
-      "lastCheckedOutBy": "Chris Evans",
-      "isCheckedOut": false
-    },
-    {
-      "id": 6,
-      "device": "Moto G",
-      "os": "Android 4.3",
-      "manufacturer": "Motorola",
-      "lastCheckedOutDate": "2016-02-21T09:10:00-05:00",
-      "lastCheckedOutBy": "Chris Evans",
-      "isCheckedOut": false
-    },
-    {
-      "id": 7,
-      "device": "Moto G",
-      "os": "Android 4.3",
-      "manufacturer": "Motorola",
-      "lastCheckedOutDate": "2016-02-21T09:10:00-05:00",
-      "lastCheckedOutBy": "Chris Evans",
-      "isCheckedOut": false
-    },
-    {
-      "id": 8,
-      "device": "Moto G",
-      "os": "Android 4.3",
-      "manufacturer": "Motorola",
-      "lastCheckedOutDate": "2016-02-21T09:10:00-05:00",
-      "lastCheckedOutBy": "Chris Evans",
-      "isCheckedOut": false
-    },
-    {
-      "id": 9,
-      "device": "Moto G",
-      "os": "Android 4.3",
-      "manufacturer": "Motorola",
-      "lastCheckedOutDate": "2016-02-21T09:10:00-05:00",
-      "lastCheckedOutBy": "Chris Evans",
-      "isCheckedOut": false
-    },
-    {
-      "id": 10,
-      "device": "Moto G",
-      "os": "Android 4.3",
-      "manufacturer": "Motorola",
-      "lastCheckedOutDate": "2016-02-21T09:10:00-05:00",
-      "lastCheckedOutBy": "Chris Evans",
-      "isCheckedOut": true
-    },
-    {
-      "id": 11,
-      "device": "Moto G",
-      "os": "Android 4.3",
-      "manufacturer": "Motorola",
-      "lastCheckedOutDate": "2016-02-21T09:10:00-05:00",
-      "lastCheckedOutBy": "Chris Evans",
-      "isCheckedOut": true
-    },
-    ]
+class App extends Component {
+    state = {
+        devices: [
+            {
+                "id": 1,
+                "device": "Derpies",
+                "os": "Android 4.3",
+                "manufacturer": "Motorola",
+                "lastCheckedOutDate": "2016-02-21T09:10:00-05:00",
+                "lastCheckedOutBy": "Chris Evans",
+                "isCheckedOut": false
+            }, {
+                "id": 2,
+                "device": "Moto G",
+                "os": "Android 4.3",
+                "manufacturer": "Motorola",
+                "lastCheckedOutDate": "2016-02-21T09:10:00-05:00",
+                "lastCheckedOutBy": "Chris Evans",
+                "isCheckedOut": false
+            }, {
+                "id": 3,
+                "device": "Moto G",
+                "os": "Android 4.3",
+                "manufacturer": "Motorola",
+                "lastCheckedOutDate": "2016-02-21T09:10:00-05:00",
+                "lastCheckedOutBy": "Chris Evans",
+                "isCheckedOut": false
+            }, {
+                "id": 4,
+                "device": "Moto G",
+                "os": "Android 4.3",
+                "manufacturer": "Motorola",
+                "lastCheckedOutDate": "2016-02-21T09:10:00-05:00",
+                "lastCheckedOutBy": "Chris Evans",
+                "isCheckedOut": false
+            }, {
+                "id": 5,
+                "device": "Moto G",
+                "os": "Android 4.3",
+                "manufacturer": "Motorola",
+                "lastCheckedOutDate": "2016-02-21T09:10:00-05:00",
+                "lastCheckedOutBy": "Chris Evans",
+                "isCheckedOut": false
+            }, {
+                "id": 6,
+                "device": "Moto G",
+                "os": "Android 4.3",
+                "manufacturer": "Motorola",
+                "lastCheckedOutDate": "2016-02-21T09:10:00-05:00",
+                "lastCheckedOutBy": "Chris Evans",
+                "isCheckedOut": false
+            }, {
+                "id": 7,
+                "device": "Moto G",
+                "os": "Android 4.3",
+                "manufacturer": "Motorola",
+                "lastCheckedOutDate": "2016-02-21T09:10:00-05:00",
+                "lastCheckedOutBy": "Chris Evans",
+                "isCheckedOut": false
+            }, {
+                "id": 8,
+                "device": "Moto G",
+                "os": "Android 4.3",
+                "manufacturer": "Motorola",
+                "lastCheckedOutDate": "2016-02-21T09:10:00-05:00",
+                "lastCheckedOutBy": "Chris Evans",
+                "isCheckedOut": false
+            }, {
+                "id": 9,
+                "device": "Moto G",
+                "os": "Android 4.3",
+                "manufacturer": "Motorola",
+                "lastCheckedOutDate": "2016-02-21T09:10:00-05:00",
+                "lastCheckedOutBy": "Chris Evans",
+                "isCheckedOut": false
+            }, {
+                "id": 10,
+                "device": "Moto G",
+                "os": "Android 4.3",
+                "manufacturer": "Motorola",
+                "lastCheckedOutDate": "2016-02-21T09:10:00-05:00",
+                "lastCheckedOutBy": "Chris Evans",
+                "isCheckedOut": true
+            }, {
+                "id": 11,
+                "device": "Moto G",
+                "os": "Android 4.3",
+                "manufacturer": "Motorola",
+                "lastCheckedOutDate": "2016-02-21T09:10:00-05:00",
+                "lastCheckedOutBy": "Chris Evans",
+                "isCheckedOut": true
+            }
+        ]
 
-  }
-  countGarageDevices = () => {
-    let count = 0
-    for(let i= 0; i<this.state.devices.length; i++){
-      if(!this.state.devices[i].isCheckedOut){
-        count = count+1
-      }
     }
-    return count
-  }
 
-
-  moveDevice = (deviceIndex) =>{
-    // console.log('1. '+ this.countGarageDevices())
-    let moveDevices = [...this.state.devices];
-    moveDevices[deviceIndex].isCheckedOut = !moveDevices[deviceIndex].isCheckedOut
-    // why is this code nessecary?
-    if(this.countGarageDevices()>10){
-    moveDevices[deviceIndex].isCheckedOut = !moveDevices[deviceIndex].isCheckedOut
-    return
+    countGarageDevices = () => {
+        let count = 0
+        for (let i = 0; i < this.state.devices.length; i++) {
+            if (!this.state.devices[i].isCheckedOut) {
+                count = count + 1
+            }
+        }
+        return count
     }
-    // why dis code ^^^^^^^^^
-    
-    // console.log('2. '+ this.countGarageDevices())
-    if(this.countGarageDevices() <= 10){
-    this.setState({devices: moveDevices}, () => {
-    this.countGarageDevices()
-    // console.log('3. '+ this.countGarageDevices())
+
+    moveDevice = (deviceIndex) => {
+        // console.log('1. '+ this.countGarageDevices())
+        let moveDevices = [...this.state.devices];
+        moveDevices[deviceIndex].isCheckedOut = !moveDevices[deviceIndex].isCheckedOut
+        // why is this code nessecary?
+        if (this.countGarageDevices() > 10) {
+            moveDevices[deviceIndex].isCheckedOut = !moveDevices[deviceIndex].isCheckedOut
+            return
+        }
+        // why dis code ^^^^^^^^^ console.log('2. '+ this.countGarageDevices())
+        if (this.countGarageDevices() <= 10) {
+            this.setState({
+                devices: moveDevices
+            }, () => {
+                this.countGarageDevices()
+                // console.log('3. '+ this.countGarageDevices())
+            })
+        }
+    }
+
+    removeDevice = (deviceIndex) => this.setState({
+        devices: this
+            .state
+            .devices
+            .slice(deviceIndex, deviceIndex + 1)
     })
-    }
-  }
 
-  removeDevice = (deviceIndex) => {
-    const devices = [...this.state.devices];
-    devices.splice(deviceIndex,1)
-    this.setState({devices: devices})
-  }
-
-  saveDevice = ()=>{
-    const devices = [...this.state.devices];
-    let addDevice = document.getElementById('add-device')
-    console.log(addDevice)
-    let newDevice = {
-      "id": addDevice.children[0].value,
-      "device": addDevice.children[1].value,
-      "os": addDevice.children[2].value,
-      "manufacturer": addDevice.children[3].value,
-      "lastCheckedOutBy": addDevice.children[4].value,
-      "lastCheckedOutDate": addDevice.children[5].value,
-      "isCheckedOut": true
-    }
-    devices.push(newDevice);
-    this.setState({devices: devices})
-    console.log(this.props)
-  }
-  render (){
-    let garageDevices = null
-    let checkoutDevices = null
-    let maxStorage = null;
-    
-    garageDevices = (<div className={classes.InGarage}>
-        {this.state.devices.map((device,index) =>{
-          if(!this.state.devices[index].isCheckedOut){
-          return (
-          <div><Device
-          className={classes.InGarage}
-          id={this.state.devices[index].id}
-          device={this.state.devices[index].device}
-          os={this.state.devices[index].os}
-          manufacturer={this.state.devices[index].manufacturer}
-          lastCheckedOutDate={this.state.devices[index].lastCheckedOutDate}
-          lastCheckedOutBy={this.state.devices[index].lastCheckedOutBy}
-          isCheckedOut = {this.state.devices[index].isCheckedOut}
-          move = {()=>this.moveDevice(index)}
-          delete ={()=>this.removeDevice(index)}
-          >
-          </Device>
-          </div>)
+    saveDevice = () => {
+        const devices = [...this.state.devices];
+        let addDevice = document.getElementById('add-device')
+        console.log(addDevice)
+        let newDevice = {
+            "id": addDevice.children[0].value,
+            "device": addDevice.children[1].value,
+            "os": addDevice.children[2].value,
+            "manufacturer": addDevice.children[3].value,
+            "lastCheckedOutBy": addDevice.children[4].value,
+            "lastCheckedOutDate": addDevice.children[5].value,
+            "isCheckedOut": true
         }
-        })}
-        </div>)
-
-  
-    checkoutDevices = (<div className={classes.InGarage}>
-        {this.state.devices.map((device,index) =>{
-          if(this.state.devices[index].isCheckedOut){
-          return (<Device
-          className={classes.InGarage}
-          id={this.state.devices[index].id}
-          device={this.state.devices[index].device}
-          os={this.state.devices[index].os}
-          manufacturer={this.state.devices[index].manufacturer}
-          lastCheckedOutDate={this.state.devices[index].lastCheckedOutDate}
-          lastCheckedOutBy={this.state.devices[index].lastCheckedOutBy}
-          isCheckedOut = {this.state.devices[index].isCheckedOut}
-          move = {()=>this.moveDevice(index)}
-          delete ={()=>this.removeDevice(index)}
-          />)
-        }
-        })}
-        </div>)
-
-    if(this.countGarageDevices()==10){
-      maxStorage = (<h1 className={classes.AlertDisplay}>Maximum Storage Capacity Reached!</h1>)      
+        devices.push(newDevice);
+        this.setState({devices: devices})
+        console.log(this.props)
     }
+    render() {
+        let garageDevices = null
+        let checkoutDevices = null
+        let maxStorage = null;
+        const inputHandler = (e, index) => (e) => {
+          let newDevice = {
+              ...this.state.devices[index]
+          }
+          newDevice[e.target.name] = e.target.value;
+              this.setState({devices : [...this
+                      .state
+                      .devices
+                      .slice(0, index),
+                  newDevice,
+                  ...this
+                      .state
+                      .devices
+                      .slice(index + 1)
+              ]})
+      };
+        garageDevices = (
+            <div className={classes.InGarage}>
+                {this
+                    .state
+                    .devices
+                    .map((device, index) => {
+                        if (!this.state.devices[index].isCheckedOut) {
+                            return (
+                                <div>
+                                    <Device
+                                        className={classes.InGarage}
+                                        
+                                        id={this.state.devices[index].id}
+                                        device={this.state.devices[index].device}
+                                        os={this.state.devices[index].os}
+                                        manufacturer={this.state.devices[index].manufacturer}
+                                        lastCheckedOutDate={this.state.devices[index].lastCheckedOutDate}
+                                        lastCheckedOutBy={this.state.devices[index].lastCheckedOutBy}
+                                        isCheckedOut={this.state.devices[index].isCheckedOut}
+                                        inputChangeHandler={(e) => inputHandler(e,index)}
+                                        move=
+                                        {()=>this.moveDevice(index)}
+                                        delete
+                                        ={() => this.removeDevice(index)}
+                                        edit={() => {
+                                        console.log(`edit button has been clicked for index ${index}`)
+                                    }}></Device>
+                                </div>
+                            )
+                        }
+                    })}
+            </div>
+        )
 
-    return (
-      <div>
-        {maxStorage}
-        <h1>In Garage</h1>
-        <div className={classes.InGarage}> 
-        {garageDevices}
-        </div>
-        <div className="notInGarage"> <h2>Has been Checked out</h2>
-        {checkoutDevices}
-        </div>
-        <div className="add-device">
-        <h1>Add Device to Checkout</h1>
-        <SavedDevices
-        className={classes.InGarage}
-        id = {null}
-        device = {null}
-        os= {null}
-        manufacturer= {null}
-        lastCheckedOutDate= {null}
-        lastCheckedOutBy= {null}
-        save = {()=>this.saveDevice()}
-        />
-        </div>
-      </div>
-      )
-  }
+        checkoutDevices = (
+            <div className={classes.InGarage}>
+                {this
+                    .state
+                    .devices
+                    .map((device, index) => {
+                        if (this.state.devices[index].isCheckedOut) {
+                            return (<Device
+                                className={classes.InGarage}
+                                id={this.state.devices[index].id}
+                                device={this.state.devices[index].device}
+                                os={this.state.devices[index].os}
+                                manufacturer={this.state.devices[index].manufacturer}
+                                lastCheckedOutDate={this.state.devices[index].lastCheckedOutDate}
+                                lastCheckedOutBy={this.state.devices[index].lastCheckedOutBy}
+                                isCheckedOut={this.state.devices[index].isCheckedOut}
+                                move=
+                                {()=>this.moveDevice(index)}
+                                delete
+                                ={() => this.removeDevice(index)}/>)
+                        }
+                    })}
+            </div>
+        )
+
+        if (this.countGarageDevices() == 10) {
+            maxStorage = (
+                <h1 className={classes.AlertDisplay}>Maximum Storage Capacity Reached!</h1>
+            )
+        }
+
+        return (
+            <div>
+                {maxStorage}
+                <h1>In Garage</h1>
+                <div className={classes.InGarage}>
+                    {garageDevices}
+                </div>
+                <div className="notInGarage">
+                    <h2>Has been Checked out</h2>
+                    {checkoutDevices}
+                </div>
+                <div className="add-device">
+                    <h1>Add Device to Checkout</h1>
+                    <SavedDevices
+                        className={classes.InGarage}
+                        id={null}
+                        device={null}
+                        os={null}
+                        manufacturer={null}
+                        lastCheckedOutDate={null}
+                        lastCheckedOutBy={null}
+                        save=
+                        {()=>this.saveDevice()}/>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default App;
-
--- src/Component/Devices/Device/Device.component.js
+++ src/Component/Devices/Device/Device.component.js
@@ -2,20 +2,21 @@ import React, { Component, useState } from 'react';
 import classes from './Device.module.css'
 
 const device = (props) => {
-  let move = props.isCheckedOut ? 'Store': 'Checkout'
+  let move = props.isCheckedOut ? 'Store': 'Checkout';
+
 
   return (
       <div className={classes.Container}>
       <div className={classes.DeviceComponent}>
       <input value ={props.id} className={classes.Device}/>
-      <input value={props.device} className={classes.Device}/>
+      <input onChange={props.inputChangeHandler} name="device" value={props.device} className={classes.Device}/>
       <input value= {props.os} className={classes.Device}/>
       <input className={classes.Device} value={props.manufacturer}/>
       <input className={classes.Device} value={props.lastCheckedOutBy}/>
       <input className={classes.Device} value = {props.lastCheckedOutDate}/>
       <button onClick ={props.move}>{move}</button>
       <button onClick = {props.delete}>Delete</button>
-      <button onClick = {props.edit}>Edit</button>
+      <button>Edit</button>
       </div>
       </div>
       )
